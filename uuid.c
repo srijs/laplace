afb2758ae542c35e68af3922836cf32783b309bf
Sam Rijs <srijs@airpost.net>
2014-05-17 09:15:35 +1000

    uuid: implement uuid_snprintf

--- uuid.c
+++ uuid.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "uuid.h"
 
 static int _uuid_parse_hex_char (char c) {
@@ -80,5 +82,17 @@ bool uuid_parse (char *grouped, uuid_t uuid) {
   }
  
   return true; 
+
+}
+
+int uuid_snprintf (char *restrict buffer, int buf_size, uuid_t uuid) {
+
+  return snprintf(buffer, buf_size, "%02x%02x%02x%02x-"
+                                    "%02x%02x-%02x%02x-%02x%02x-"
+                                    "%02x%02x%02x%02x%02x%02x",
+                                    uuid[0],  uuid[1],  uuid[2],  uuid[3],
+                                    uuid[4],  uuid[5],  uuid[6],  uuid[7],
+                                    uuid[8],  uuid[9],  uuid[10], uuid[11],
+                                    uuid[12], uuid[13], uuid[14], uuid[15]);
 
 }
--- uuid.h
+++ uuid.h
@@ -8,4 +8,6 @@ typedef uint8_t uuid_t[16];
 
 bool uuid_parse (char *grouped, uuid_t uuid);
 
+int uuid_snprintf (char *restrict buffer, int buf_size, uuid_t uuid);
+
 #endif
